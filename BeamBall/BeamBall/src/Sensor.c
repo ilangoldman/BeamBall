c400ca1b3562e530685a0e28dc875019a932bcb9
Pc-Cliente\ilangoldman <ilan_goldman@hotmail.com>
2017-09-08 19:27:52 -0300

    ATSAM3N family documentation

--- BeamBall/BeamBall/src/Sensor.c
+++ BeamBall/BeamBall/src/Sensor.c
@@ -9,17 +9,45 @@
 #include "conf_board.h"
 #include "conf_clock.h"
 
+//****** ADC **********//
+
 #define ADC_CLOCK	6400000
 
+//****** Timer Counter **********//
 #define TC			TC0
 #define CHANNEL		0
 #define ID_TC		ID_TC0
 #define TC_IRQn     TC0_IRQn
 
+//****** I/O pin Sensor **********//
+#define TRIG 0
+#define TRIG_PIN ?
+
+
+//****** Usando o doc do Sensor **********//
+
+void Start_Sensor() {
+	// Set trigger pin high to start the measure
+	TRIG_PIN |= (1<<TRIG);
+	// Sensor is busy
+	Busy_measure =1 ;
+	// Delay 10us to sensor finish measure
+	//delay(10);
+	// Set trigger pin to low
+	TRIG_PIN &= ~(1<<TRIG);
+}
+
+
+//****** Usando Conversor ADC **********//
+
+
 // TODO !!
-int readSensor() {
+int readSensor(uint32_t ADC_value) {
 	// ler o valor do sensor (pos da bola em relacao a viga)
-	 
+
+	// Formula
+	// value = ticks / 58; // cm
+	
 	return 0;
 }
 
@@ -30,8 +58,10 @@ void ADC_Handler (void) {
 		// Get latest digital data value from ADC and can be used by application
 		uint32_t result = adc_get_latest_value(ADC);
 
-		// Add PID calculation logic here
+		// O result vai sempre retornar o valor maximo??
 		
+		// Add PID calculation logic here
+		// resultCM = readSensor (result);
 		
 		// double ul_duty =  (result*100)/4092;
 		// pwm_update_duty(ul_duty);
@@ -43,7 +73,7 @@ void adc_setup(void) {
 	pmc_enable_periph_clk(ID_ADC);
 	adc_init(ADC, sysclk_get_main_hz(), ADC_CLOCK , 8);
 	adc_configure_timing(ADC, 0, ADC_SETTLING_TIME_3, 1);
-	//adc_set_resolution(ADC, ADC_MR_LOWRES_BITS_12); // define not found ??
+	adc_set_resolution(ADC, ADC_MR_LOWRES_BITS_10);
 	adc_enable_channel(ADC, ADC_CHANNEL_5);
 	adc_enable_interrupt(ADC, ADC_IER_DRDY);
 	adc_configure_trigger(ADC, ADC_TRIG_SW, 0);
@@ -53,6 +83,8 @@ void adc_setup(void) {
 void TC0_Handler(void) {
 	tc_get_status(TC,CHANNEL);
 	adc_start(ADC);
+	//ticks++;
+	// count num of tick from ADC start until ready
 }
 
 static void tc_setup(void)
